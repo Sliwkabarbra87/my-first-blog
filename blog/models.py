23e3594c4fd93f80750acadf1c22d0fd72551e65
Basia 8718 <barbara.sliwinska87@gmial.com>
2019-09-28 17:22:14 +0200

    Modified templates to display posts from database.

--- blog/models.py
+++ blog/models.py
@@ -6,13 +6,13 @@ class Post(models.Model):
     title = models.CharField(max_length=200)
     text = models.TextField()
     created_date = models.DateTimeField(default=timezone.now)
-    publiszed_date = models.DateTimeField(blank=True, null=True)
+    published_date = models.DateTimeField(blank=True, null=True)
 
     def publish(self):
         self.published_date =timezone.now()
         self.save()
 
-        def __str__(self):
-            return self.title
+    def __str__(self):
+        return self.title
 
 # Create your models here.
